c159431c7cb1dcd1df8b428f5292451aa773e588
telmo <telmoalexandre2505@gmail.com>
2023-08-15 15:13:38 +0100

    Finalized the extraction of the Navbar

--- src/App.js
+++ src/App.js
@@ -8,23 +8,16 @@ import ContactPage from './views/Contact';
 import SignInPage from './views/SignIn';
 import RegisterPage from './views/Register';
 import NotFoundPage from './views/404';
+import PageLayout from './components/PageLayout';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <div className="content">
-          <Routes>
-            <Route exact path='/' element={<LandingPage />}/>
-            <Route path='/home' element={<HomePage />}/>
-            <Route path='/create' element={<CreateReviewPage />}/>
-            <Route path='/about' element={<AboutUsPage />}/>
-            <Route path='/contact' element={<ContactPage />}/>
-            <Route path='/signin' element={<SignInPage />}/>
-            <Route path='/register' element={<RegisterPage />}/>
-            <Route path='*' element={<NotFoundPage />}/>
-          </Routes>
-        </div>
+        <Routes>
+          <Route exact path='/' element={<LandingPage />}/>
+          <Route path='/*' element={<PageLayout />}/>
+        </Routes>
       </div>
     </Router>
     
---
+++ src/components/PageLayout.js
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import HomePage from '../views/Home';
+import CreateReviewPage from '../views/CreateReview';
+import AboutUsPage from '../views/AboutUs';
+import ContactPage from '../views/Contact';
+import SignInPage from '../views/SignIn';
+import RegisterPage from '../views/Register';
+import NotFoundPage from '../views/404';
+
+const PageLayout = () => {
+    return ( 
+        <>
+            <Navbar />
+            <div className="content">
+                <Routes>
+                    <Route path='/home' element={<HomePage />}/>
+                    <Route path='/create' element={<CreateReviewPage />}/>
+                    <Route path='/about' element={<AboutUsPage />}/>
+                    <Route path='/contact' element={<ContactPage />}/>
+                    <Route path='/signin' element={<SignInPage />}/>
+                    <Route path='/register' element={<RegisterPage />}/>
+                    <Route path='*' element={<NotFoundPage />}/>
+                </Routes>
+            </div>
+        </>
+    );
+}
+ 
+export default PageLayout;

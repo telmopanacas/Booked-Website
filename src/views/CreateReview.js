6e56732009913e428be6994e5cfe3856fffd18fa
telmo <telmoalexandre2505@gmail.com>
2023-08-16 16:44:53 +0100

    The form part is 99% done.

--- src/views/CreateReview.js
+++ src/views/CreateReview.js
@@ -1,6 +1,37 @@
+import '../assets/styles/CreateReview.css'
+import FormInput from '../components/FormInput.js'
+import { useState } from 'react';
+import FormTextArea from '../components/FormTextArea';
+import FormUsernameAndRating from '../components/FormUsernameAndRating';
+
 const CreateReviewPage = () => {
+    const [postTitle, setPostTitle] = useState('');
+    const [bookTitle, setBookTitle] = useState('');
+    const [bookAuthor, setBookAuthor] = useState('');
+    const [username, setUsername] = useState('');
+    const [rating, setRating] = useState('0');
+    const [review, setReview] = useState('');
+
     return ( 
-        <h1>Create Review Page</h1>
+        <div className="create-page">
+            
+            <div className="left-side">
+                
+            </div>
+            <div className="right-side">
+                <div className="form-div">
+                    <form>
+                        <FormInput label="Title" placeholder="Post title" setInput={setPostTitle}/>
+                        <FormInput label="Book title" placeholder="Book Title" setInput={setBookTitle}/>
+                        <FormInput label="Book author" placeholder="Book Author" setInput={setBookAuthor}/>
+                        <FormUsernameAndRating setUsername={setUsername} setRating={setRating}/>
+                        <FormTextArea label="Review" placeholder="Write your review." setInput={setReview}/>
+                    </form>
+                </div>
+                
+            </div>
+        
+        </div>
     );
 }
  

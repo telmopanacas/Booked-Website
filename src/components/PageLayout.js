84c7a2b2e798f425a37fae66623fdae48c6bb6d0
telmo <telmoalexandre2505@gmail.com>
2023-09-13 15:40:50 +0100

    Implemented route protection in the application

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -6,6 +6,7 @@ import AboutUsPage from '../views/AboutUs';
 import ContactPage from '../views/Contact';
 import SignInPage from '../views/SignIn';
 import NotFoundPage from '../views/404';
+import RequireAuth from "./RequireAuth";
 
 const PageLayout = () => {
     return ( 
@@ -14,7 +15,9 @@ const PageLayout = () => {
             <div className="content">
                 <Routes>
                     <Route path='/home' element={<HomePage />}/>
-                    <Route path='/create' element={<CreateReviewPage />}/>
+                    <Route element= {<RequireAuth />}>
+                        <Route path='/create' element={<CreateReviewPage />}/>
+                    </Route>
                     <Route path='/about' element={<AboutUsPage />}/>
                     <Route path='/contact' element={<ContactPage />}/>
                     <Route path='/signin' element={<SignInPage />}/>
